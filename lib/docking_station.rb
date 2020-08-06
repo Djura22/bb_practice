630fa6f3fbb452723c4c354d159adee0895ffba1
JRG <47917431+JR-G@users.noreply.github.com>
2020-08-06 21:11:15 +0100

    Completed step 15 with SRP

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -6,15 +6,26 @@ class DockingStation
   end
 
 	def release_bike
-    fail 'No bikes available' if @bikes.empty?
+    fail 'No bikes available' if empty?
     @bikes.pop
   end
 
 	def dock(bike)
-		fail 'Docking station full' if @bikes.length >= 20
+		fail 'Docking station full' if full?
 		@bikes << bike
-	end
+  end
+  
+  attr_reader :bikes
 
-	attr_reader :bikes
+  private
+
+  def full?
+    @bikes.length >= 20
+  end
+
+  def empty?
+    @bikes.empty?
+  end
+	
 
 end
